ca74d80a8e6a45eaf5e92d112e997600d1e998e7
maraple87 <46070085+maraple87@users.noreply.github.com>
2022-08-31 09:43:05 +0200

    Update Dateidurchsuchung.ps1

--- Dateidurchsuchung/Dateidurchsuchung.ps1
+++ Dateidurchsuchung/Dateidurchsuchung.ps1
@@ -1,7 +1,8 @@
-﻿#wählt die aktuellste Datei aus
+#wählt die aktuellste Datei aus
 $logdatei = Get-ChildItem ReportServerService* | Sort CreationTime -Descending | select -First 1 
 #Logdateinamen speichern
 $name = Get-ChildItem ReportServerService*  -name | Sort CreationTime -Descending | select -First 1 
+#$name = Select-String -path $logdatei
 
 #Durchsucht eine Datei nach allen Fehlermeldungen
 $body = Select-String -path $logdatei -Pattern 'error' 
@@ -14,6 +15,7 @@ $body = $body -replace ";",";`n`n"
 
 #zur Hilfe für weitere Bearbeitung in Textdatei speichern
 $body.trim() >> tmp.txt
+
 
 
 $nl = [Environment]::NewLine
@@ -51,9 +53,15 @@ $body = Select-String -path tmp2.txt -pattern 'nicht unterst*' -notmatch  | ForE
 #Da der Pfad + Datename bei select-String mit abgespeichert wird, entfernen wir ihn
 $body = $body -replace ".*txt:",""
 
+
+$nl = [Environment]::NewLine
+
 #Leere Zeile zwischen den Fehlermeldungen
 #$body = $body -replace ";",";`n`n"
 $body.trim() >> tmp3.txt
+
+
+
 #---------------------------------------------
 
 #alle mit den Meldungen über den Webserver
@@ -69,7 +77,9 @@ $body.trim() >> tmp3.txt
 
 
 #Überschrift 
-$body =  "LOGDATEI: " + $name + " `n" +  "Server: al-sql-04`n" + "Pfad: C:\Program Files\Microsoft SQL Server\MSRS13.MSSQLSERVER\Reporting Services\LogFiles`n" + "---------------------------------------------------------`n" +" `n" +  "FEHLERMELDUNGEN: `n" + " `n" + " `n" + $body 
+#$body =  "LOGDATEI: " + $name + " `n" +  "Server: al-sql-04`n" + "Pfad: C:\Program Files\Microsoft SQL Server\MSRS13.MSSQLSERVER\Reporting Services\LogFiles`n" + "---------------------------------------------------------`n" +" `n" +  "FEHLERMELDUNGEN: `n" + " `n" + " `n" + $body 
+$body =  "LOGDATEI: " + $name + " `n" +  "Server: al-sql-04`n" + "Pfad: \\al-sql-04\c$\Program Files\Microsoft SQL Server\MSRS13.MSSQLSERVER\Reporting Services\LogFiles\" + $name + "`n" + "---------------------------------------------------------`n" +" `n" +  "FEHLERMELDUNGEN: `n" + " `n" + " `n" + $body 
+
 
 #Wenn die Überprüfung wahr, dann wird eine Mail vershickt
 if( Select-String -path $logdatei -SimpleMatch "error")
@@ -77,8 +87,9 @@ if( Select-String -path $logdatei -SimpleMatch "error")
 #SMTP-Server in einer variablen Speichern
 $EmailServer = "smtprelay.intern.stimme.de "
 #Email verschicken mit angehängter Datei ($logdatei)
-Send-MailMessage -to "martin.plewik@stimme.de" -from "PowerShell <report@stimme.de>" -Subject "Auswertung Reportlogdateien" -body "$body" -SmtpServer $EmailServer 
+Send-MailMessage -to "martin.plewik@stimme.de", "thomas.mayenberger@stimme.de" -from "PowerShell <report@stimme.de>" -Subject "Auswertung Reportlogdateien" -body "$body" -SmtpServer $EmailServer 
 }
+#, "thomas.mayenberger@stimme.de"
 
 #Die Textdateien noch löschen
 Remove-Item tmp.txt
